ad8bdaca78f67f074c0e9aa6b294dabe30e036d2
alessandrabjlobo <alessandrabjlobo@gmail.com>
2025-10-25 23:41:21 -0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 // src/components/Sidebar.tsx
 import { useEffect, useMemo, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import { useUserRole, type UserRole } from "@/hooks/useUserRole";
 import {
   Building2,
   LayoutDashboard,
